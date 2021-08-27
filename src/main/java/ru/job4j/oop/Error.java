8219bc94fe9daf43ba7a0b786a6a898dd27bb6c5
Viktor <mannar@inbox.ru>
2021-08-27 13:25:28 +0500

    oop Error [1]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+int status;
+String message;
+
+public  Error () {
+}
+
+public  Error ( boolean active, int status, String message){
+    this.active = active;
+    this.status = status;
+    this.message = message;
+}
+
+public  void  print () {
+    System.out.println(active + " " + status + " " + message);
+}
+public static void main(String[] args) {
+Error error = new Error();
+error.print();
+Error nopaje = new Error(true, 404, "страница отсутствует");
+nopaje.print();
+Error test = new Error(true, -1, "не знаю, что тут написать");
+test.print();
+}
+}
