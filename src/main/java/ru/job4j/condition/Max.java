f6ec5fffdd2374e6c5457391bf13c136657c08ea
Viktor <mannar@inbox.ru>
2021-08-31 03:59:06 +0500

    OPP Max [3]                oop Max [2]    Max [1]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,15 +2,16 @@ package ru.job4j.condition;
 
 public class Max {
 
-    public  static  int add (int a, int b){
+    public static int max(int a, int b) {
         return Math.max(a, b);
     }
 
-    public static int add(int a, int b, int c){
-        return Math.max(c, add(a, b));
+    public static int max(int a, int b, int c) {
+        return max(c, max(a, b));
     }
 
-    public  static int add ( int a, int b, int c, int d){
-        return  Math.max(d, add(a, b, c));
+    public static int max(int a, int b, int c, int d) {
+        return max(d, max(a, b, c));
     }
+
 }
