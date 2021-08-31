ea802c5d229ad8977cf9cbcc92e7eff69cd94798
Viktor <mannar@inbox.ru>
2021-08-31 05:21:29 +0500

    OOp Points 3D [1]

--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -24,9 +24,9 @@ public class Point {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
-//    public double distance3d(Point that) {
-//    return
-//    }
+    public double distance3d(Point that) {
+    return sqrt(pow(this.x - that.x, 2)+ pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
 
     public static void main(String[] args) {
         Point a = new Point(0, 0);
