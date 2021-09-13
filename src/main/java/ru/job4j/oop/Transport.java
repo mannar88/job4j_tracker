fb10487a2754a68d723bbb8757b356af2bddade5
Viktor <mannar@inbox.ru>
2021-09-13 12:38:35 +0500

    3.2. Зачем нужно приведение типов[1]

--- src/main/java/ru/job4j/oop/Transport.java
+++ src/main/java/ru/job4j/oop/Transport.java
@@ -1,4 +1,13 @@
 package ru.job4j.oop;
 
 public class Transport {
+    public static void main(String[] args) {
+        Vehicle airplane = new Airplane();
+        Vehicle train = new Train();
+        Vehicle bus = new Bus();
+    Vehicle [] transports = new  Vehicle[] {airplane, train, bus};
+        for (Vehicle arra:transports) {
+            arra.move();
+        }
+    }
 }
