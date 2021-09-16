1d553c8530120507ea96eb8ff9a203fdd4eac98f
Viktor <mannar@inbox.ru>
2021-09-16 23:20:07 +0500

    9.2. Тесты вывода на консоль[1]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -52,4 +52,97 @@ public class StartUITest {
         assertThat(tracker.findById(item.getId()), is(nullValue()));
     }
 
+    @Test
+    public void whenExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitMenu(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu." + System.lineSeparator() +
+                        "0. Выход" + System.lineSeparator()
+        ));
+    }
+
+    @Test
+public  void thenFindItem() {
+Output output =new StubOutput();
+Tracker tracker = new Tracker();
+    Input input = new StubInput(new  String[]{
+        "0", "0", "1"
+    });
+    tracker.add(new Item("test"));
+UserAction [] userActions = {
+        new FindItem(output),
+        new ExitMenu(output)
+};
+new  StartUI(output).init(input, tracker, userActions);
+    assertThat(output.toString(), is("Menu." + System.lineSeparator()
+    + "0. Поиск заявки по ID" + System.lineSeparator()
+    + "1. Выход" +System.lineSeparator()
+    + "=== Find item by id ====" + System.lineSeparator()
+    + "Enter id: " + System.lineSeparator()
+                    + "Заявка с введенным id: " + "0"  + " не найдена." + System.lineSeparator()
+                    + "Menu." + System.lineSeparator()
+                    + "0. Поиск заявки по ID" + System.lineSeparator()
+                    + "1. Выход" +System.lineSeparator()
+    ));
+    }
+
+    @Test
+public  void thenFindItemsByName() {
+        Output output = new StubOutput();
+        Tracker tracker = new Tracker();
+        Input input = new StubInput(new  String[]{
+                "0",
+                "test",
+                "1"
+        });
+    UserAction [] userActions = {
+            new FindItemsByName(output),
+            new ExitMenu(output)
+    };
+new  StartUI(output).init(input, tracker, userActions);
+assertThat(output.toString(), is(
+        "Menu." + System.lineSeparator()
+        +"0. Поииск заявки по имени" + System.lineSeparator()
+            + "1. Выход" +System.lineSeparator()
++ "=== Find items by name ====" +System.lineSeparator()
++ "Enter name: " + System.lineSeparator()
++ "Заявки с именем: " + "test" + " не найдены." + System.lineSeparator()
++ "Menu." + System.lineSeparator()
+                    +"0. Поииск заявки по имени" + System.lineSeparator()
+                    + "1. Выход" +System.lineSeparator()
+    ));
+    }
+
+@Test
+    public  void thenShowAllItems() {
+    Tracker tracker = new Tracker();
+    Output output = new StubOutput();
+    Input input = new StubInput(new  String[] {
+        "0",
+            "1"
+    });
+UserAction [] userActions = {
+        new ShowAllItems(output),
+        new ExitMenu(output)};
+new  StartUI(output).init(input, tracker, userActions);
+assertThat(output.toString(), is(
+        "Menu." + System.lineSeparator()
++ "0. Ввывод всех заявок." + System.lineSeparator()
+        + "1. Выход" + System.lineSeparator()
++ "=== Show all items ====" + System.lineSeparator()
++ "Хранилище еще не содержит заявок" + System.lineSeparator()
++ "Menu." + System.lineSeparator()
+                + "0. Ввывод всех заявок." + System.lineSeparator()
+                + "1. Выход" + System.lineSeparator()
+));
+    }
+
 }
