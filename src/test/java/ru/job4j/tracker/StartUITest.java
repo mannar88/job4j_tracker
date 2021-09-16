9846770015b45e7cbb3ce99011b0321ed28fdaa5
Viktor <mannar@inbox.ru>
2021-09-16 06:14:51 +0500

    9.1 Зависимость от System.out[2]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -43,8 +43,8 @@ public class StartUITest {
         Item item = tracker.add(new Item("Deleted item"));
         Input in = new StubInput(
             new String[] {"0" , String.valueOf(item.getId()), "1"}
-        );1
-UserAction[ ] actions = {
+        );
+        UserAction[ ] actions = {
                 new DeleteItem(new ConsoleOutput()),
                 new ExitMenu(new ConsoleOutput())
         };
