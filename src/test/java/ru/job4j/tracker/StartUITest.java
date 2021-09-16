430a2ab927a5f70b86758a3c5cdf630f80763c44
Viktor <mannar@inbox.ru>
2021-09-16 23:52:43 +0500

    9.2. Тесты вывода на консоль[4]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
+
 public class StartUITest {
 
     @Test
