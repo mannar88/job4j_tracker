fb7985890e605f829e228b8caa7f9b9ef6828b01
Viktor <mannar@inbox.ru>
2021-09-25 15:15:39 +0500

    3. Реализация профессий в коде[3]

--- src/main/java/ru/job4j/profession/Profession.java
+++ src/main/java/ru/job4j/profession/Profession.java
@@ -7,7 +7,14 @@ private  String surname;
 private  String education;
 private  String birthday;
 
-public  String  getName (){
+    public Profession(String name, String surname, String education, String birthday) {
+        this.name = name;
+        this.surname = surname;
+        this.education = education;
+        this.birthday = birthday;
+    }
+
+    public  String  getName (){
 return name;
 }
 
