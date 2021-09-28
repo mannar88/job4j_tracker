b91578a7c24406fc513a697fa277ca45c6343653
Viktor <mannar@inbox.ru>
2021-09-28 16:57:07 +0500

    1.3.3. Map. HashMap. LinkedHashMap[1]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,55 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map <User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+User key = this.findByPassport(passport);
+if (key != null &&!users.get(key).contains(account)) {
+users.get(key).add(account);
+}
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if ( (user.getPassport().equals(passport))) {
+                return  user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+User user = this.findByPassport(passport);
+if (user != null) {
+for (Account account : users.get(user)) {
+if (account.getRequisite().equals(requisite)) {
+    return  account;
+}
+}
+}
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        Account srcAccount = this.findByRequisite(srcPassport, srcRequisite);
+        Account deftAccount = this.findByRequisite(destPassport, destRequisite);
+        if (srcAccount!=null && deftAccount != null && srcAccount.getBalance() >= amount) {
+srcAccount.setBalance(srcAccount.getBalance() - amount);
+deftAccount.setBalance(deftAccount.getBalance() + amount);
+rsl = true;
+        }
+            return rsl;
+    }
+}
