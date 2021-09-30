117421a60734d4b41f06ee5921bb7aef2c50be5f
Viktor <mannar@inbox.ru>
2021-09-30 20:31:49 +0500

    2. Что такое JavaDoc

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -5,13 +5,31 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Класс эмулирует банковский процесс: добавление клиента, привязка счетов, перевод денег счета на счет
+ * @author Viktor Burdin
+ * @version1.0
+ */
 public class BankService {
+    /**
+     * Хранение учетных записей клиента в  коллекции - тип
+     HashMap, приведенный в List     */
+
     private final Map <User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Метод проверяет наличие клиента в коллекции и если запись отсутсутсвует - добавляет в коллекцию
+     * @param user - пользователь
+     */
     public void addUser(User user) {
 users.putIfAbsent(user, new ArrayList<Account>());
     }
 
+    /**
+     * Метод привязывает счета к клиенту и добавляет их в коллекцию
+     * @param passport - номер паспорта для поиска клиента
+     * @param account - счет клиента
+     */
     public void addAccount(String passport, Account account) {
 User key = findByPassport(passport);
 if (key != null &&!users.get(key).contains(account)) {
@@ -19,6 +37,11 @@ users.get(key).add(account);
 }
     }
 
+    /**
+     * Метод исчет в коллекции конкретного юзера по паспорту
+     * @param passport - данные паспорта
+     * @return - на выходе учетная запись клиента
+     */
     public User findByPassport(String passport) {
         for (User user : users.keySet()) {
             if (user.getPassport().equals(passport)) {
@@ -28,6 +51,12 @@ users.get(key).add(account);
         return null;
     }
 
+    /**
+     * Метод исчет список счетов по , у конкретного пользователя
+      * @param passport - данные паспорта
+     * @param requisite - счет
+     * @return- ссписок счетов, если такие найдутся
+     */
     public Account findByRequisite(String passport, String requisite) {
 User user = findByPassport(passport);
 if (user != null) {
@@ -39,6 +68,16 @@ if (account.getRequisite().equals(requisite)) {
 }
         return null;
     }
+
+    /**
+     * метод переводит деньги из одного счета, на другой.
+     * @param srcPassport - данные паспорта, от куда перевод.
+     * @param srcRequisite - счет, от куда перевод.
+     * @param destPassport - данные пасорта, куда перевод
+     * @param destRequisite -счет, куда перевод
+     * @param amount - сумма перевод
+     * @return - true, если перевод совершен, false, если нет
+     */
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
