6782fb6454b863ea8438c8cc2e92b8eda19b57aa
Viktor <mannar@inbox.ru>
2021-10-23 01:04:23 +0500

    0. Stream API [#58166] 0 3

---
+++ src/main/java/ru/job4j/stream/StreamNum.java
@@ -0,0 +1,12 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamNum {
+public  static  List <Integer> sortPositive (List <Integer> list){
+return list.stream().filter(
+        i -> i%2 == 0
+).collect(Collectors.toList());
+}
+}
---
+++ src/test/java/ru/job4j/stream/StreamNumTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+
+public class StreamNumTest {
+@Test
+public    void  positive (){
+    List <Integer> list = List.of(-2, -1, 0, 1,2);
+    assertThat(StreamNum.sortPositive(list), is(List.of(-2, 0,2)));
+}
+
+}
