646edb06cf7cd69884b881b026927cbcd8ed8c91
Viktor <mannar@inbox.ru>
2021-10-23 01:23:05 +0500

    0. Stream API [#58166] 0 32

--- src/main/java/ru/job4j/stream/StreamNum.java
+++ src/main/java/ru/job4j/stream/StreamNum.java
@@ -4,8 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamNum {
-public  static  Listst <Integer> sortPositive (List <Integer> list){
-return list.streamam().filter(
+public  static  List <Integer> sortPositive (List <Integer> list){
+return list.stream().filter(
         i -> i> 0
 ).collect(Collectors.toList());
 }
