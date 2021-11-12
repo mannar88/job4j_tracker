74ddf05c31982ebd618c91ad4173c088542d7e8f
Viktor <mannar@inbox.ru>
2021-11-12 18:25:26 +0500

    2. Аттестация [#49777] 0 8 2

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -1,6 +1,7 @@
 package ru.job4j.stream;
 
 import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -27,10 +28,9 @@ return  stream
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
         return stream
                 .flatMap(p -> p.getSubjects().stream())
-                .collect(Collectors.groupingBy(Subject::getName, Collectors.averagingDouble(Subject::getScore)))
+                .collect(Collectors.groupingBy(Subject::getName,  LinkedHashMap::new, Collectors.averagingDouble(Subject::getScore)))
                 .entrySet().stream()
                 .map(e -> new Tuple(e.getKey(), e.getValue()))
-                .sorted((l, r) -> r.getName().compareTo(l.getName()))
                 .collect(Collectors.toList());
     }
     public static Tuple bestStudent(Stream<Pupil> stream) {
